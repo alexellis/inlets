d61a240c2b133797d22e925ecffcf5f18ad72497
Pavel Makarenko <cryfall@gmail.com>
2019-02-19 23:20:05 +0300

    migrating to urfave/cli
    Signed-off-by: Pavel Makarenko <cryfall@gmail.com>

--- main.go
+++ main.go
@@ -1,90 +1,138 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/alexellis/inlets/pkg/client"
 	"github.com/alexellis/inlets/pkg/server"
+
+	"github.com/urfave/cli"
 )
 
-// Args parsed from the command-line
-type Args struct {
-	Port              int
-	Server            bool
-	Remote            string
-	Upstream          string
-	GatewayTimeoutRaw string
-	GatewayTimeout    time.Duration
-	Token             string
-	PrintServerToken  bool
-}
+var (
+	Version = "dev"
+)
 
 func main() {
-	args := Args{}
-	flag.IntVar(&args.Port, "port", 8000, "port for server")
-	flag.BoolVar(&args.Server, "server", true, "server or client")
-	flag.StringVar(&args.Remote, "remote", "127.0.0.1:8000", " server address i.e. 127.0.0.1:8000")
-	flag.StringVar(&args.Upstream, "upstream", "", "upstream server i.e. http://127.0.0.1:3000")
-	flag.StringVar(&args.GatewayTimeoutRaw, "gateway-timeout", "5s", "timeout for upstream gateway")
-	flag.StringVar(&args.Token, "token", "", "token for authentication")
-	flag.BoolVar(&args.PrintServerToken, "print-token", true, "prints the token in server mode")
+	// Moving to cobra
+	app := cli.NewApp()
+	app.Version = Version
+	app.Name = "inlets"
+	app.HelpName = "inlets"
+	app.Usage = `Expose your local endpoints to the Internet
 
-	flag.Parse()
+Start the tunnel server on a machine with a publicly-accessible IPv4 IP address such as a VPS
+	inlets server --port80
 
-	argsUpstreamParser := ArgsUpstreamParser{}
+Start the tunnel client
+	inlets client --remote 192.168.0.101:80 --upstream http://127.0.0.1:3000
+`
 
-	upstreamMap := map[string]string{}
+	app.Commands = []cli.Command{
+		{
+			Name:  "server",
+			Usage: "start the tunnel server on a machine with a publicly-accessible IPv4 IP address such as a VPS",
+			Flags: []cli.Flag{
+				cli.IntFlag{
+					Name:  "port",
+					Value: 8000,
+					Usage: "port for server",
+				},
+				cli.StringFlag{
+					Name:  "token",
+					Value: "",
+					Usage: "token for authentication",
+				},
+				cli.StringFlag{
+					Name:  "gateway-timeout",
+					Value: "5s",
+					Usage: "timeout for upstream gateway",
+				},
+				cli.BoolFlag{
+					Name:  "print-token",
+					Usage: "prints the token in server mode",
+				},
+			},
+			Action: func(ctx *cli.Context) error {
+				port := ctx.Int("port")
+				token := ctx.String("token")
+				timeout := ctx.String("gateway-timeout")
+				printToken := ctx.Bool("print-token")
 
-	if args.Server == false {
+				if len(token) > 0 && printToken {
+					log.Printf("Server token: %s", token)
+				}
 
-		if len(args.Upstream) == 0 {
-			log.Printf("give --upstream\n")
-			return
-		}
-		upstreamMap = argsUpstreamParser.Parse(args.Upstream)
-		for key, val := range upstreamMap {
-			log.Printf("Upstream: %s => %s\n", key, val)
-		}
+				gatewayTimeout, gatewayTimeoutErr := time.ParseDuration(timeout)
+				if gatewayTimeoutErr != nil {
+					fmt.Printf("%s\n", gatewayTimeoutErr)
+					return nil
+				}
+
+				log.Printf("Gateway timeout: %f secs\n", gatewayTimeout.Seconds())
+
+				s := server.Server{
+					Port:           port,
+					GatewayTimeout: gatewayTimeout,
+					Token:          token,
+				}
+				s.Serve()
+				return nil
+			},
+		},
+
+		{
+			Name:  "client",
+			Usage: "start the tunnel client",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "remote",
+					Value: "127.0.0.1:8000",
+					Usage: "server address i.e. 127.0.0.1:8000",
+				},
+				cli.StringFlag{
+					Name:  "upstream",
+					Value: "",
+					Usage: "upstream server i.e. http://127.0.0.1:3000, http://127.0.0.1:3001",
+				},
+				cli.StringFlag{
+					Name:  "token",
+					Value: "",
+					Usage: "token for authentication",
+				},
+			},
+			Action: func(ctx *cli.Context) error {
+				remote := ctx.String("remote")
+				upstream := ctx.String("upstream")
+				token := ctx.String("token")
+
+				if len(upstream) == 0 {
+					log.Printf("give --upstream\n")
+					return nil
+				}
+
+				argsUpstreamParser := ArgsUpstreamParser{}
+				upstreamMap := argsUpstreamParser.Parse(upstream)
+
+				for key, val := range upstreamMap {
+					log.Printf("Upstream: %s => %s\n", key, val)
+				}
+
+				c := client.Client{
+					Remote:      remote,
+					UpstreamMap: upstreamMap,
+					Token:       token,
+				}
+
+				return c.Connect()
+			},
+		},
 	}
 
-	if args.Server {
-
-		if len(args.Token) > 0 && args.PrintServerToken {
-			log.Printf("Server token: %s", args.Token)
-		}
-
-		gatewayTimeout, gatewayTimeoutErr := time.ParseDuration(args.GatewayTimeoutRaw)
-		if gatewayTimeoutErr != nil {
-			fmt.Printf("%s\n", gatewayTimeoutErr)
-			return
-		}
-
-		args.GatewayTimeout = gatewayTimeout
-		log.Printf("Gateway timeout: %f secs\n", gatewayTimeout.Seconds())
-	}
-
-	if args.Server {
-		server := server.Server{
-			Port:           args.Port,
-			GatewayTimeout: args.GatewayTimeout,
-			Token:          args.Token,
-		}
-		server.Serve()
-
-	} else {
-		client := client.Client{
-			Remote:      args.Remote,
-			UpstreamMap: upstreamMap,
-			Token:       args.Token,
-		}
-
-		err := client.Connect()
-
-		if err != nil {
-			panic(err)
-		}
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
 	}
 }
